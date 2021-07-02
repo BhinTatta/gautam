2c99a7f7345893dee148806a411559e5ff5aad70
BhinTatta <67475518+BhinTatta@users.noreply.github.com>
2021-07-02 23:42:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2800); // Change image every 2 seconds
+}
+
+//reeview/testimonial
+var scroll_offset = 120;
+$(window).scroll(function(){
+	var $this = $(window);
+	if( $('.sticky-btn').length ) {
+		if( $this.scrollTop() > scroll_offset ) { 
+			$('.sticky-btn').addClass('revealed');
+		} else {
+			$('.sticky-btn').removeClass('revealed');
+		}	
+	}
+});
