584e60194c0f107903872bc8529856fba934cd8a
BhinTatta <67475518+BhinTatta@users.noreply.github.com>
2021-07-03 18:50:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2800); // Change image every 2 seconds
+}
+
+//reeview/testimonial
+var scroll_offset = 120;
+$(window).scroll(function(){
+	var $this = $(window);
+	if( $('.sticky-btn').length ) {
+		if( $this.scrollTop() > scroll_offset ) { 
+			$('.sticky-btn').addClass('revealed');
+		} else {
+			$('.sticky-btn').removeClass('revealed');
+		}	
+	}
+});
+
+
+//rerad  more----------------read more
+let noOfCharac = 110;
+let contents = document.querySelectorAll(".content");
+contents.forEach(content => {
+    //If text length is less that noOfCharac... then hide the read more button
+    if(content.textContent.length < noOfCharac){
+        content.nextElementSibling.style.display = "none";
+    }
+    else{
+        let displayText = content.textContent.slice(0,noOfCharac);
+        let moreText = content.textContent.slice(noOfCharac);
+        content.innerHTML = `${displayText}<span class="dots">...</span><span class="hide more">${moreText}</span>`;
+    }
+});
+
+function readMore(btn){
+    let post = btn.parentElement;
+    post.querySelector(".dots").classList.toggle("hide");
+    post.querySelector(".more").classList.toggle("hide");
+    btn.textContent == "Read More" ? btn.textContent = "Read Less" : btn.textContent = "Read More";
+}
